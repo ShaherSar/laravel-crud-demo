85842e567d7531af55756b32ab859398e553ec3f
Shaher Sarairah <shahersar@icloud.com>
2023-09-07 09:35:13 +0300

    add mail function

--- app/Services/EmailService/SmtpEmailService.php
+++ app/Services/EmailService/SmtpEmailService.php
@@ -4,7 +4,7 @@ namespace App\Services\EmailService;
 
 class SmtpEmailService implements EmailService{
     public function sendTextEmail($to, $subject, $content): void {
-
+        mail($to, $subject, $content);
     }
 
     public function sendHtmlEmail($to, $subject, $content){
